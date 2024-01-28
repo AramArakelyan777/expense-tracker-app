9991a3818cac67153677d8ddde7928ff5f24f7a7
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-28 12:43:57 +0400

    added lexend fonts

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
-import logo from "../../assets/logo.png"
+import logo from "../../assets/img/logo.png"
 
 export default function Navbar() {
     return (
@@ -9,6 +9,11 @@ export default function Navbar() {
             <NavLink to="/">
                 <img alt="logo" src={logo} className="logo" />
             </NavLink>
+            <div className="navbar-links">
+                <NavLink to="/expenses">Expenses</NavLink>
+                <NavLink to="/user">User</NavLink>
+                <NavLink to="/login">Login</NavLink>
+            </div>
         </nav>
     )
 }
