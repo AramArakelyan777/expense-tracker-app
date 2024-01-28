8e994f31382ea2a31ccae18a6cb247c494f38e11
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-28 12:20:17 +0400

    added logo to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
+import logo from "../../assets/logo.png"
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <NavLink to="/">Logo</NavLink>
+            <NavLink to="/">
+                <img alt="logo" src={logo} className="logo" />
+            </NavLink>
         </nav>
     )
 }
