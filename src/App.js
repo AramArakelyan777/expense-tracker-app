3f51fbe883c77ccdbfecb99398b78863ab0ae093
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-28 12:11:29 +0400

    created some routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./components/Home/HomePage"
+import Navbar from "./components/Navbar"
 
 function App() {
     return (
         <div className="App">
+            <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
             </Routes>
