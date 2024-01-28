99440d745e866d7736a95cb7b09eb013f793b928
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-28 12:09:25 +0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./components/Home/HomePage"
 
 function App() {
     return (
         <div className="App">
-            Initial
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
         </div>
     )
 }
