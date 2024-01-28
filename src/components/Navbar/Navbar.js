4a43f96baac5bb36d6e57a89f7b207f5093e6605
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-28 14:43:39 +0400

    created tooltips

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 import logo from "../../assets/img/logo.png"
 import { useAuth } from '../Login/auth'
+import Tippy from '@tippy.js/react'
+import "tippy.js/dist/tippy.css"
 
 export default function Navbar() {
     const auth = useAuth()
@@ -12,8 +14,16 @@ export default function Navbar() {
                 <img alt="logo" src={logo} className="logo" />
             </NavLink>
             <div className="navbar-links">
-                <NavLink to="/expenses">Expenses</NavLink>
-                <NavLink to="/user">User</NavLink>
+                {!auth.user ?
+                    <Tippy content="Log in first to visit the expenses page" delay={500}>
+                        <NavLink to="/expenses">Expenses</NavLink>
+                    </Tippy> : <NavLink to="/expenses">Expenses</NavLink>}
+
+                {!auth.user ?
+                    <Tippy content="Log in first to visit the user page" delay={500}>
+                        <NavLink to="/user">User</NavLink>
+                    </Tippy> : <NavLink to="/user">User</NavLink>}
+
                 {!auth.user && (
                     <NavLink to="/login">Login</NavLink>
                 )}
