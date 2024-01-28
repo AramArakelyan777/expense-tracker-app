fad25f4d2cc55be082322337bdda3d60dad92546
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-28 14:24:02 +0400

    logout logic and styles

--- src/components/Login/User.js
+++ src/components/Login/User.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "./auth"
+import "./Login.css"
 
-export default function User() {
-  return (
-    <div>User</div>
-  )
+function User() {
+    const auth = useAuth()
+    const navigate = useNavigate()
+
+    return (
+        <div>
+            <h1 className="logoutHeader">Logout and return when you need us.</h1>
+            <p>Thank you for trusting us with your financial journey, {auth.user}.</p>
+            <button className="logOut" onClick={() => {
+                auth.logout()
+                navigate("/")
+            }}>Log Out</button>
+        </div>
+    )
 }
+
+export default User
