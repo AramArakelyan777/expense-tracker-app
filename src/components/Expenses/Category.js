d837ce0b54cdb717d0ac2230a61b1dcaf0cb1157
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-29 14:11:38 +0400

    added expense value changes context

--- src/components/Expenses/Category.js
+++ src/components/Expenses/Category.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer } from 'react'
 import "../../assets/text.css"
 import Expense from './Expense'
 import "./Expenses.css"
@@ -13,10 +13,6 @@ const expenseReducer = (state, action) => {
 export default function Category({ name }) {
     const [expensesList, dispatchExpensesList] = useReducer(expenseReducer, [])
 
-    const [date, setDate] = useState(null)
-    const [expenseAmmount, setExpenseAmmount] = useState(0)
-    const [description, setDescription] = useState("")
-
     return (
         <div className="oneCategory">
             <h2 className='second-header'>{name} expenses</h2>
@@ -24,23 +20,20 @@ export default function Category({ name }) {
 
             <form>
                 <input type="date" className='date input-light' onChange={(evt) => {
-                    setDate(evt.target.value)
-                    console.log(evt.target.value)
+
                 }} /><br />
 
                 <input type="number" placeholder='Expense ammount' className='expense input-light' onChange={(evt) => {
-                    setExpenseAmmount(evt.target.value)
-                    console.log(evt.target.value)
+
                 }} /><br />
 
                 <textarea className='description input-light' placeholder='Small description' onChange={(evt) => {
-                    setDescription(evt.target.value)
-                    console.log(evt.target.value)
+
                 }} /><br />
 
                 <button className="addAnExpense" onClick={(evt) => {
                     evt.preventDefault()
-                    dispatchExpensesList({ type: "ADD_AN_EXPENSE", payload: { date, expenseAmmount, description } })
+                    dispatchExpensesList({ type: "ADD_AN_EXPENSE", payload: {} })
                 }}>Add an expense</button>
             </form>
 
---
+++ src/components/Expenses/ExpenseContext/ExpenseContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from "react"
+
+export const ExpenseContext = createContext(null)
+
+export const ExpenseContextProvider = ({ children }) => {
+    const [date, setDate] = useState(null)
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            console.log('Date value changed:', date);
+        }, 1000);
+
+        return () => clearTimeout(timeoutId);
+    }, [date])
+
+    const handleDateChange = (e) => {
+        setDate(e.target.value)
+    }
+
+
+    const [expenseAmmount, setExpenseAmmount] = useState(0)
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            console.log('Expense ammount value changed:', expenseAmmount);
+        }, 1000)
+
+        return () => clearTimeout(timeoutId)
+    }, [expenseAmmount])
+
+    const handleAmmountChange = (e) => {
+        setExpenseAmmount(e.target.value)
+    }
+
+
+    const [description, setDescription] = useState("")
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            console.log('Description value changed:', description);
+        }, 1000)
+
+        return () => clearTimeout(timeoutId)
+    }, [description])
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value)
+    }
+
+
+    return <ExpenseContext.Provider value={{ handleDateChange, handleAmmountChange, handleDescriptionChange }}>{children}</ExpenseContext.Provider>
+}
