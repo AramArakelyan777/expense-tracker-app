0434a48093c48408ef9833403fcd0f25e0ffbe89
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-02-01 19:07:33 +0400

    responsive navbar, final version

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
 import "./Navbar.css"
 import logo from "../../assets/img/logo.png"
+import smallLogo from "../../assets/img/smallLogo.png"
 import { useAuth } from "../Login/auth"
 import Tippy from "@tippy.js/react"
 import "tippy.js/dist/tippy.css"
@@ -9,10 +10,28 @@ import "tippy.js/dist/tippy.css"
 export default function Navbar() {
     const auth = useAuth()
 
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 600)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 600)
+        }
+
+        window.addEventListener("resize", handleResize)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
     return (
         <nav className="navbar">
             <NavLink to="/">
-                <img alt="logo" src={logo} className="logo" />
+                <img
+                    alt="logo"
+                    src={isSmallScreen ? smallLogo : logo}
+                    className="logo"
+                />
             </NavLink>
             <div className="navbar-links">
                 {!auth.user ? (
