35dada95866a778a68df31a627b906614a5922a0
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-03-18 13:53:57 +0400

    code formatting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,20 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+        options: {
+            storySort: (a, b) =>
+                a.id === b.id
+                    ? 0
+                    : a.id.localeCompare(b.id, undefined, { numeric: true }),
+        },
     },
-    options: {
-      storySort: (a, b) =>
-        a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
-    },
-  },
-};
+}
 
-export default preview;
+export default preview
